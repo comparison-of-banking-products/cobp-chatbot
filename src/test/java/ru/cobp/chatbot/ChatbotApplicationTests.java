668cde18717d8a5a7af1c99cacd0730e8c24c45e
Ab <104671142+alex-blag@users.noreply.github.com>
2024-02-11 11:27:42 +0200

    feat: add Spring Boot App

---
+++ src/test/java/ru/cobp/chatbot/ChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cobp.chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
